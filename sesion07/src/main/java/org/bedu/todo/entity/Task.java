eb0abc96691c491e28324d211986d3e14eadd387
Jorge Ramón <jorge.ramon2128@gmail.com>
2024-05-28 19:55:56 -0600

    Ejemplo de conexion con base de datos

---
+++ sesion07/src/main/java/org/bedu/todo/entity/Task.java
@@ -0,0 +1,22 @@
+package org.bedu.todo.entity;
+
+public class Task {
+  
+  private int id;
+  private String description;
+
+  public int getId() {
+    return id;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
